f7302eab40718862664f4c4253eb1609cfbc557d
Fire_Queen <842830543@qq.com>
2024-12-27 13:58:39 +0800

    地址簿功能代码开发

---
+++ sky-server/src/main/java/com/sky/service/AddressBookService.java
@@ -0,0 +1,26 @@
+package com.sky.service;
+
+/**
+ * Description:
+ *
+ * @Author 张恒
+ * @Version
+ */
+import com.sky.entity.AddressBook;
+import java.util.List;
+
+public interface AddressBookService {
+
+    List<AddressBook> list(AddressBook addressBook);
+
+    void save(AddressBook addressBook);
+
+    AddressBook getById(Long id);
+
+    void update(AddressBook addressBook);
+
+    void setDefault(AddressBook addressBook);
+
+    void deleteById(Long id);
+
+}
