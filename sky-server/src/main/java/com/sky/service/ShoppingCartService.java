545e73cf5a33191c498f21052e8492c9286874cc
Fire_Queen <842830543@qq.com>
2024-12-27 13:39:00 +0800

    查看购物车业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,26 @@
+package com.sky.service;
+
+/**
+ * Description:
+ *
+ * @Author 张恒
+ * @Version
+ */
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import java.util.List;
+
+public interface ShoppingCartService {
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
+}
